bf2a8e976effd047faf3dd71670d1a5c1b2b69dc
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-09-13 15:46:05 +0600

    fix: consumer service

--- src/main/consumer/consumer.service.ts
+++ src/main/consumer/consumer.service.ts
@@ -12,73 +12,83 @@ export class ConsumerService {
     serviceId: string,
     createConsumerDto: AcceptBid,
   ) {
-    // check if the user is the owner of the service request
-    const validConsumer = await this.prisma.service.findFirst({
-      where: { id: serviceId, userId: userid },
-    });
-    if (!validConsumer) {
-      throw new BadRequestException('You are not the owner of this service');
+    try {
+      // check if the user is the owner of the service request
+      const validConsumer = await this.prisma.service.findFirst({
+        where: { id: serviceId, userId: userid },
+      });
+      if (!validConsumer) {
+        throw new BadRequestException('You are not the owner of this service');
+      }
+      const serviceRequest = await this.prisma.bid.findFirst({
+        where: {
+          AND: [
+            { serviceId },
+            { serviceProviderId: createConsumerDto.serviceProviderId },
+          ],
+        },
+      });
+      if (!serviceRequest) {
+        throw new BadRequestException(
+          'Service request not with this service provider id ',
+        );
+      }
+      // check if the bid is already accepted
+      if (serviceRequest.status === 'ACCEPTED') {
+        throw new BadRequestException('Bid is already accepted');
+      }
+      // update the bid status to accepted
+      const updatedBid = await this.prisma.bid.update({
+        where: { id: serviceRequest.id },
+        data: { status: 'ACCEPTED' },
+      });
+      const assingedServiceRequest = await this.prisma.service.update({
+        where: { id: serviceId },
+        data: {
+          assignedServiceProviderId: createConsumerDto.serviceProviderId,
+        },
+      });
+      return {
+        message: 'Bid accepted successfully',
+        data: { updatedBid, assingedServiceRequest },
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
-    const serviceRequest = await this.prisma.bid.findFirst({
-      where: {
-        AND: [
-          { serviceId },
-          { serviceProviderId: createConsumerDto.serviceProviderId },
-        ],
-      },
-    });
-    if (!serviceRequest) {
-      throw new BadRequestException(
-        'Service request not with this service provider id ',
-      );
-    }
-    // check if the bid is already accepted
-    if (serviceRequest.status === 'ACCEPTED') {
-      throw new BadRequestException('Bid is already accepted');
-    }
-    // update the bid status to accepted
-    const updatedBid = await this.prisma.bid.update({
-      where: { id: serviceRequest.id },
-      data: { status: 'ACCEPTED' },
-    });
-    const assingedServiceRequest = await this.prisma.service.update({
-      where: { id: serviceId },
-      data: { assignedServiceProviderId: createConsumerDto.serviceProviderId },
-    });
-    return {
-      message: 'Bid accepted successfully',
-      data: { updatedBid, assingedServiceRequest },
-    };
   }
 
   async serviceComplete(userid: string, serviceId: string) {
-    // check if the user is the owner of the service request
-    const validConsumer = await this.prisma.service.findFirst({
-      where: { id: serviceId, userId: userid },
-    });
-    if (!validConsumer) {
-      throw new BadRequestException('You are not the owner of this service');
+    try {
+      // check if the user is the owner of the service request
+      const validConsumer = await this.prisma.service.findFirst({
+        where: { id: serviceId, userId: userid },
+      });
+      if (!validConsumer) {
+        throw new BadRequestException('You are not the owner of this service');
+      }
+      // check if the service is assigned to a service provider
+      if (!validConsumer.assignedServiceProviderId) {
+        throw new BadRequestException(
+          'Service is not assigned to any service provider',
+        );
+      }
+      // check if the service is already completed
+      if (validConsumer.isCompleteFromConsumer === true) {
+        throw new BadRequestException('Service is already completed');
+      }
+      // update the service status to completed
+      const updatedService = await this.prisma.service.update({
+        where: { id: serviceId },
+        data: { isCompleteFromConsumer: true },
+      });
+      return { message: 'Service completed successfully', updatedService };
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
-    // check if the service is assigned to a service provider
-    if (!validConsumer.assignedServiceProviderId) {
-      throw new BadRequestException(
-        'Service is not assigned to any service provider',
-      );
-    }
-    // check if the service is already completed
-    if (validConsumer.isCompleteFromConsumer === true) {
-      throw new BadRequestException('Service is already completed');
-    }
-    // update the service status to completed
-    const updatedService = await this.prisma.service.update({
-      where: { id: serviceId },
-      data: { isCompleteFromConsumer: true },
-    });
-    return { message: 'Service completed successfully', updatedService };
   }
+
   async giveReview(userid: string, serviceId: string, body: SendReviewDto) {
     try {
-      console.log(body);
       // check if the user is the owner of the service request
       const validConsumer = await this.prisma.user.findFirst({
         where: { id: userid, role: 'CONSUMER' },
@@ -90,6 +100,7 @@ export class ConsumerService {
       const validServiceProvider = await this.prisma.serviceProvider.findFirst({
         where: { userId: serviceId },
       });
+      console.log(validServiceProvider);
       if (!validServiceProvider) {
         throw new BadRequestException('Invalid service provider');
       }
@@ -108,7 +119,7 @@ export class ConsumerService {
           },
         },
       });
-
+      console.log(existingReview);
       if (existingReview) {
         throw new BadRequestException('You already reviewed this provider');
       }
@@ -121,7 +132,7 @@ export class ConsumerService {
           serviceProviderId: validServiceProvider.id,
         },
       });
-
+      console.log(averageRating, totalUsers);
       await this.prisma.serviceProvider.update({
         where: { id: validServiceProvider.id },
         data: {
