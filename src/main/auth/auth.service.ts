94db2b246d5967622da2dbf10a56a9f1d083a407
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-11-08 01:26:24 +0600

    Refactor: Move isVerifiedFromAdmin check to service provider block
    This commit moves the `isVerifiedFromAdmin` check inside the
    `if` block that checks for `SERVICE_PROVIDER` role.  This ensures
    that only service providers are subject to this verification check.

--- src/main/auth/auth.service.ts
+++ src/main/auth/auth.service.ts
@@ -218,12 +218,13 @@ export class AuthService {
       const serviceProvider = await this.prisma.serviceProvider.findFirst({
         where: { userId: userExists.id },
       });
-      if (!serviceProvider?.isVerifiedFromAdmin) {
-        this.logger.log(`Service provider ${loginDto.email} is verified by admin`);
-        throw new UnauthorizedException('Your account is not verified by admin yet, Provide valid documents and try again later');
-      }
+
       const token = await this.helperService.createTokenEntry(userExists.id, payload);
       if (userExists.role === UserRole.SERVICE_PROVIDER) {
+        if (!serviceProvider?.isVerifiedFromAdmin) {
+          this.logger.log(`Service provider ${loginDto.email} is verified by admin`);
+          throw new UnauthorizedException('Your account is not verified by admin yet, Provide valid documents and try again later');
+        }
         return ApiResponse.success(
           { token, serviceProvider },
           'Service provider logged in successfully',
